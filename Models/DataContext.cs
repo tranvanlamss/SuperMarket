a11e6764e6eb540811f8635c1e38e38f35182b3a
tranvanlamss <lamt63356@gmail.com>
2021-10-27 10:06:10 +0700

    Add project files.

---
+++ Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace SuperMarket.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("SuperMarket")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        
+
+    }
+}
